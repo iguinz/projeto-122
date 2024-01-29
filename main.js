3160a4cbf2d40075c89c7a2edac09235445525e1
iguinz <130095170+iguinz@users.noreply.github.com>
2024-01-29 15:35:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+x = 0;
+y = 0;
+
+drawApple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "O sistema está ouvindo. Por favor, fale.";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "A fala foi reconhecida: " + content; 
+
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(drawApple == "set")
+  {
+    document.getElementById("status").innerHTML = toNumber + " maçãs desenhadas";
+    drawApple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+
+    synth.speak(utterThis);
+
+    speakData = "";
+}
+
+let screenWidth = 0;
+let screenHeight = 0;
+let apple = '';
+let speakData = '';
+let toNumber = '';
+
+function preload() {
+    
+    apple = loadImage('apple.png');
+}
+
